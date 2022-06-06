96e8384778b294b28067e8486959b240d6aabe93
mishio-n <doncool340@gmail.com>
2022-06-06 23:32:48 +0900

    :dizzy: ポイントのカウントアップアニメーション追加

--- pages/2022-2023/odds/index.tsx
+++ pages/2022-2023/odds/index.tsx
@@ -2,9 +2,12 @@ import { type Owner } from ".prisma/client";
 import { GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
+import { createRef, useEffect, useRef } from "react";
 import { groupBy } from "../../../lib/group-by";
 import { prisma } from "../../../lib/prisma";
 import { aggregateRacePoint } from "../../../lib/race-point";
+import { range } from "../../../lib/range";
+import { gsap, Power3 } from "gsap";
 
 type Props = {
   ownerWithPoints: (Owner & { totalPoint: number })[];
@@ -36,6 +39,26 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
 const Odds2022_2023: NextPage<
   InferGetStaticPropsType<typeof getStaticProps>
 > = ({ ownerWithPoints }) => {
+  const pointRefs = useRef(
+    range(0, ownerWithPoints.length - 1).map(() => createRef<HTMLSpanElement>())
+  );
+
+  // カウントアップアニメーション
+  useEffect(() => {
+    pointRefs.current.forEach((pointRef) => {
+      const point = pointRef.current?.dataset.point!;
+      let obj = { count: 0 };
+      gsap.to(obj, {
+        count: point,
+        ease: "power3.inOut",
+        duration: 3.6,
+        onUpdate: () => {
+          pointRef.current!.textContent = Math.floor(obj.count).toString();
+        },
+      });
+    });
+  }, []);
+
   return (
     <>
       <Head>
@@ -59,15 +82,30 @@ const Odds2022_2023: NextPage<
           </ul>
         </div>
         <div className="artboard p-5">
-          {ownerWithPoints.map((owner) => (
-            <div className="flex" key={owner.id}>
-              <Link href={`/2022-2023/odds/${owner.id}`}>
-                <a>
-                  <span>{owner.name}</span>
-                  <span>{owner.totalPoint}</span>
-                </a>
-              </Link>
-            </div>
+          {ownerWithPoints.map((owner, index) => (
+            <Link href={`/2022-2023/odds/${owner.id}`} key={owner.id}>
+              <div
+                className={`max-w-sm py-4 px-1 pb-0 mt-4 flex items-center justify-between ${
+                  index === 0
+                    ? "border-[#f7ef8e] border-b-4"
+                    : index === 1
+                    ? "border-[#cdd5e0] border-b-[3px]"
+                    : "border-gray-300 border-b-2"
+                }`}
+              >
+                <div className="card-title">{owner.name}</div>
+                <div className="">
+                  <span
+                    className="point text-xl font-bold font-mono"
+                    data-point={owner.totalPoint}
+                    ref={pointRefs.current[index]}
+                  >
+                    {owner.totalPoint}
+                  </span>
+                  <span className="ml-2">ポイント</span>
+                </div>
+              </div>
+            </Link>
           ))}
         </div>
       </div>
