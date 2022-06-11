4b95c792001a4e0e9a171f375c79d6e8e2fb7113
mishio-n <doncool340@gmail.com>
2022-06-12 01:40:51 +0900

    :mag: metaタグ追加

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,9 @@ export default function Document() {
         <link rel="apple-touch-icon" href="/icon.png"></link>
         <meta name="theme-color" content="#374151" />
         <meta name="icon" content="/favicon.ico" />
+        <meta property="og:title" content={"おうちPOG"} />
+        <meta property="og:description" content={"POG集計サイト"} />
+        <meta property="og:image" content={"/favicon.ico"} />
       </Head>
       <body className="bg-slate-50" style={{ minHeight: "100vh" }}>
         <Header />
