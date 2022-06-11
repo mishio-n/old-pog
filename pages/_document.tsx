4ec939452bc54b438be8392802bb9e92a7a12a56
mishio-n <doncool340@gmail.com>
2022-06-12 01:24:04 +0900

    :bento: PWAファイルを追加

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,15 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 import { Header } from "~/components/Header";
 
 export default function Document() {
   return (
     <Html lang="ja" data-theme="cupcake">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png"></link>
+        <meta name="theme-color" content="#fff" />
+        <meta name="icon" content="/favicon.ico" />
+      </Head>
       <body className="bg-slate-50" style={{ minHeight: "100vh" }}>
         <Header />
         <Main />
