6152f54cef6203c5b57146dce6189e40d091f479
mishio-n <doncool340@gmail.com>
2022-06-26 14:00:05 +0900

    集計ロジック修正

--- pages/2022-2023/dart/index.tsx
+++ pages/2022-2023/dart/index.tsx
@@ -24,7 +24,7 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
     id,
     name,
     totalPoint: horsesByOwner[id].reduce(
-      (result, horse) => result + aggregateRacePoint(horse.race).totalPoint,
+      (result, horse) => result + aggregateRacePoint(horse.race).totalBasePoint,
       0
     ),
   }));
