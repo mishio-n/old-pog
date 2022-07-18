29c58d2c491ad5e6cccc46f3e587bcfcd7a043c5
mishio-n <doncool340@gmail.com>
2022-07-18 15:53:47 +0900

    開発時のPrismaログを有効化

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,26 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 declare global {
-  var prisma: PrismaClient;
+  var prisma: PrismaClient<Prisma.PrismaClientOptions, Prisma.LogLevel>;
 }
 
-let prisma: PrismaClient;
+let prisma: PrismaClient<Prisma.PrismaClientOptions, Prisma.LogLevel>;
 
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
   if (!global.prisma) {
-    global.prisma = new PrismaClient();
+    global.prisma = new PrismaClient({
+      log: ["query"],
+    });
   }
   prisma = global.prisma;
+
+  prisma.$on("query", (e) => {
+    console.log("Query: " + e.query);
+    console.log("Params: " + e.params);
+    console.log("Duration: " + e.duration + "ms");
+  });
 }
+
 export { prisma };
